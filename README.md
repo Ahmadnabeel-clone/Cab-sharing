6507dc06867e42c2412d5653367c5c77ba90fc9d
Ahmad Nabeel <ahmadnabeel3314@gmail.com>
2024-07-10 13:53:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is a simple project for students who want to help people by cab sharing or carpooling with those in need.
+# CabSharingApp
+CS253 Project
---
+++ routes/login.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require("../config/auth");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const emailvalidator = require("email-validator");
+const validateDate = require("validate-date");
+const jsonParser = bodyParser.json();
+// Load User model
+const User = require("../models/User");
+const Travel = require("../models/travel");
+
+router.get("/", forwardAuthenticated, (req, res) => res.render("login"));
+
+router.post("/", (req, res, next) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (!user) {
+      passport.authenticate("local", {
+        successRedirect: "/dashboard",
+        failureRedirect: "/login",
+        failureFlash: true,
+      })(req, res, next);
+    }else{
+      if(user.required == "Student"){
+        passport.authenticate("local", {
+          successRedirect: "/dashboard",
+          failureRedirect: "/login",
+          failureFlash: true,
+        })(req, res, next);
+      }else{
+        passport.authenticate("local", {
+          successRedirect: "/dashboarddriver",
+          failureRedirect: "/login",
+          failureFlash: true,
+        })(req, res, next);
+
+
+      }
+    }
+      
+    });
+});
+
+module.exports = router;
